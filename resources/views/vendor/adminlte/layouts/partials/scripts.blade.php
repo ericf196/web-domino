e4895674c57f8fc29fd34aa913917e65620e6f34
Eric <ericf196@gmail.com>
2017-11-11 18:27:32 -0400

    version estable

---
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -0,0 +1,16 @@
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
+<!-- jQuery 2.1.4 -->
+<script src="{{ url('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+
+{{-- <script src="{{ url('/js/bootstrap.min.js') }}" type="text/javascript"></script> --}}
+<!-- AdminLTE App -->
+<script src="{{ url('/js/plusis.js') }}" type="text/javascript"></script>
+
+<script src="{{ url (mix('/js/app.js')) }}" type="text/javascript"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+      Both of these plugins are recommended to enhance the
+      user experience. Slimscroll is required when using the
+      fixed layout. -->
---
+++ resources/views/vendor/adminlte/layouts/partials/scripts_auth.blade.php
@@ -0,0 +1,11 @@
+<!-- Compiled app javascript -->
+
+<!-- jQuery 2.1.4 -->
+<script src="{{ url('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ url('/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- iCheck -->
+<script src="{{ url('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
+
+<script src="{{ url (mix('/js/app.js')) }}"></script>
+
